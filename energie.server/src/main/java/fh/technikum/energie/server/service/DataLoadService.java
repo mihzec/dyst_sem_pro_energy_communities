c5e2777ccce5194b8b9c8b3743980ca723101100
mihael <zecevic.mihael@gmx.at>
2026-04-19 18:56:05 +0200

    return dto instead of list<entity> - use helper methode to summarize history data values

--- energie.server/src/main/java/fh/technikum/energie/server/service/DataLoadService.java
+++ energie.server/src/main/java/fh/technikum/energie/server/service/DataLoadService.java
@@ -1,12 +1,15 @@
 package fh.technikum.energie.server.service;
 
+import fh.technikum.energie.server.dto.HistoryDataDto;
 import fh.technikum.energie.server.entity.HistoryData;
 import fh.technikum.energie.server.repository.HistoryDataRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.function.Function;
 
 @Service
 public class DataLoadService {
@@ -18,7 +21,22 @@ public class DataLoadService {
     }
 
     @Transactional
-    public List<HistoryData> loadHistoryData(LocalDateTime start, LocalDateTime end) {
-        return this.historyDataRepository.findByTimestampHourBetween(start,end);
+    public HistoryDataDto loadHistoryData(LocalDateTime start, LocalDateTime end) {
+        //get data by timeperiod
+        List<HistoryData> historyDataList = this.historyDataRepository.findByTimestampHourBetween(start, end);
+
+        //sum results -> methode wird aufgerufen mit (list, HistoryData Objekt :: die getMethode ())
+        BigDecimal communityProduced = sumValuesFromList(historyDataList, HistoryData::getCommunityProduced);
+        BigDecimal communityUsed = sumValuesFromList(historyDataList, HistoryData::getCommunityUsed);
+        BigDecimal gridUsed = sumValuesFromList(historyDataList, HistoryData::getGridUsed);
+
+        return new HistoryDataDto(communityProduced, communityUsed, gridUsed);
+    }
+
+    //methode - iteriert über liste , holt je nach getterMetode jeweiligen wert -> ZERO, wenn nicht vorhanden, sonst addiert
+    private BigDecimal sumValuesFromList(List<HistoryData> list, Function<HistoryData, BigDecimal> getterMethode) {
+        return list.stream()
+                .map(getterMethode)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
