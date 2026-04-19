29c2a38a2adeae70957182ede140bfc4e83054ed
mihael <zecevic.mihael@gmx.at>
2026-04-19 12:17:56 +0200

    add fields, DataViewModel calls and initialize()

--- energie.gui/src/main/java/fh/technikum/energie/gui/controller/MainViewController.java
+++ energie.gui/src/main/java/fh/technikum/energie/gui/controller/MainViewController.java
@@ -1,11 +1,61 @@
 package fh.technikum.energie.gui.controller;
 
+import fh.technikum.energie.gui.viewmodel.CurrentDataViewModel;
+import fh.technikum.energie.gui.viewmodel.HistoryDataViewModel;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
 public class MainViewController {
 
-    public void onRefreshClicked() {
+    @FXML
+    private Label outputCommunityPoolValue;
+    @FXML
+    private Label outputGridPortionValue;
 
+    @FXML
+    private ComboBox<String> startTimeDatePicker;
+    @FXML
+    private ComboBox<String> endTimeDatePicker;
+
+    @FXML
+    private Label historyOutputProduced;
+    @FXML
+    private Label historyOutputUsed;
+    @FXML
+    private Label historyOutputGridUsed;
+
+    CurrentDataViewModel currentDataViewModel = new CurrentDataViewModel();
+    HistoryDataViewModel historyDataViewModel = new HistoryDataViewModel();
+
+    //click-methode für aktuelle daten
+    public void onRefreshClicked() {
+        currentDataViewModel.loadCurrentData();
     }
 
+    //click-methode für historische daten
     public void onShowDataClicked() {
+        historyDataViewModel.loadHistoryData();
+    }
+
+    @FXML
+    public void initialize() {
+        startTimeDatePicker.getEditor().setEditable(false);
+        endTimeDatePicker.getEditor().setEditable(false);
+
+        outputCommunityPoolValue.textProperty().bind(currentDataViewModel.communityPoolOutputProperty());
+        outputGridPortionValue.textProperty().bind(currentDataViewModel.gridPortionOutputProperty());
+
+        historyOutputProduced.textProperty().bind(historyDataViewModel.historyOutputProducedProperty());
+        historyOutputUsed.textProperty().bind(historyDataViewModel.historyOutputUsedProperty());
+        historyOutputGridUsed.textProperty().bind(historyDataViewModel.historyOutputGridUsedProperty());
+
+        startTimeDatePicker.valueProperty().bindBidirectional(historyDataViewModel.startTimeProperty());
+        endTimeDatePicker.valueProperty().bindBidirectional(historyDataViewModel.endTimeProperty());
+
+        //load current-data and history-data with localdatetime.now() on initialization
+        currentDataViewModel.loadCurrentData();
+        historyDataViewModel.loadHistoryData();
     }
 }
