bdb6c88cc11deae20fd3314f3b2a41d6bb22b39f
mihael <zecevic.mihael@gmx.at>
2026-04-19 19:23:57 +0200

    call local backend springboot app via common callServerByURI methode and map result to object via jackson-databinding

--- energie.gui/src/main/java/fh/technikum/energie/gui/service/DataLoadService.java
+++ energie.gui/src/main/java/fh/technikum/energie/gui/service/DataLoadService.java
@@ -1,9 +1,17 @@
 package fh.technikum.energie.gui.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fh.technikum.energie.gui.model.CurrentData;
 import fh.technikum.energie.gui.model.HistoryData;
+import fh.technikum.energie.gui.util.Constants;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.LocalDateTime;
 
 public class DataLoadService {
@@ -14,7 +22,31 @@ public class DataLoadService {
     }
 
     public HistoryData loadHistoryData(LocalDateTime start, LocalDateTime end) {
-        return new HistoryData(new BigDecimal("10.50"), new BigDecimal("20.5"), new BigDecimal("30.5")); //dummy data
-        //TODO: server call for history data
+        try {
+            String serverHistoryURI = String.format("%s/energy/historical?start=%s&end=%s",
+                    //folgende params verden statt dem %s gesetzt -> reihenfolge wichtig
+                    Constants.URL_LOCALHOST, //localhost url als constante, damit wiederverwendet werden kann
+                    start,
+                    end);
+
+            String response = callServerByURI(serverHistoryURI).body();
+            return mapResponseToHistoryData(response);
+        } catch (Exception e) {
+            System.out.println("exception in loadHistoryData: " + e.getMessage());
+            return null; //no data available or error
+        }
+    }
+
+    private HttpResponse<String> callServerByURI(String serverUri) throws IOException, InterruptedException {
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(URI.create(serverUri)).GET().build();
+        HttpClient client = HttpClient.newBuilder().build();
+        return client.send(getRequest, HttpResponse.BodyHandlers.ofString());
+    }
+
+    private HistoryData mapResponseToHistoryData(String response) throws JsonProcessingException {
+        //mapping with jackson -> dependency notwendig
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(response, HistoryData.class);
     }
 }
