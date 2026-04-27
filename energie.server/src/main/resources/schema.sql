117dcdd86958f73639d2fa59d015fe8cf68fa697
ahmed <wi24b159@technikum-wien.at>
2026-04-27 22:42:15 +0200

    fix schema - currendata to 2 digits

--- energie.server/src/main/resources/schema.sql
+++ energie.server/src/main/resources/schema.sql
@@ -14,6 +14,6 @@ CREATE TABLE current_data
 (
     id                 BIGINT AUTO_INCREMENT  PRIMARY KEY,
     timestamp_hour     TIMESTAMP NOT NULL,
-    community_depleted DECIMAL(10, 3),
-    grid_portion       DECIMAL(10, 3)
+    community_depleted DECIMAL(10, 2),
+    grid_portion       DECIMAL(10, 2)
 );
