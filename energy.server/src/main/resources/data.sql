ddc9a01c6881a9522b6b3dd90bc7251a56eab149
mihael <zecevic.mihael@gmx.at>
2026-06-04 18:25:10 +0200

    database connections - rm unused

--- energy.server/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO history_data (timestamp_hour, community_produced, community_used, grid_used)
-VALUES ('2025-01-10 14:00:00', 18.05, 18.02, 1.056),
-       ('2025-01-10 13:00:00', 15.015, 14.033, 2.049);
-
-INSERT INTO current_data (timestamp_hour, community_depleted, grid_portion)
-VALUES ('2025-01-10 14:00:00', 100.00, 5.63);
